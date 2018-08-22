12e19e062d653504a601b4a2a7d0b08adc3be43b
javoor1 <javoor1@hotmail.com>
2018-08-22 18:44:05 -0500

    Post Model, Post Validations, Post/User Associations, User Authentication, tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,36 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show]
+
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		puts params.inspect
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		if @post.save
+			redirect_to @post, notice: "Your post was created succesfully"
+		else
+			render :new
+		end	
+	end
+
+	def show
+		@post = Post.find_by_id(params[:id])
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:date, :rationale)
+	end
+
+	def set_post
+		@post = Post.find_by_id(params[:id])
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
+	#Associations
+	has_many :posts
+
+
+	#Authorization
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  #Validations
   validates :first_name, :last_name, presence: true       
 end
