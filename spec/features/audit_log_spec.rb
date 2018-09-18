3f3807f7e95133beee5a11687613d53e8f2c6379
javoor1 <javoor1@hotmail.com>
2018-09-17 23:00:27 -0500

    test for blockin non admin users from Audit Log Index

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -4,9 +4,10 @@ describe 'AuditLog Feauture' do
 	
 	describe 'index' do 
 		before do
+			@admin_user = FactoryGirl.create(:admin_user)
+			@non_admin_user = FactoryGirl.create(:user)
+      login_as(@admin_user, :scope => :user)
 			FactoryGirl.create(:audit_log)
-			admin_user = FactoryGirl.create(:admin_user)
-      login_as(admin_user, :scope => :user)
 		end	
 		it 'can be reached successfully' do
 			visit audit_logs_path
@@ -15,11 +16,14 @@ describe 'AuditLog Feauture' do
 
 	  it 'renders audit log content' do 
 	  	visit audit_logs_path
-	  	# byebug
 	  	expect(page).to have_content(/JON/)
+	  end
+
+	  it 'can not be accesed by non admin' do 
+	  	logout(@admin_user)
+	  	login_as(@non_admin_user, scope: :user)
+	  	visit audit_logs_path
+			expect(current_path).to eq(root_path)
 	  end
 	end
 end
-
-
-
