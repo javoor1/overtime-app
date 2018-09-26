a88f3b5eb913b7cce8643a3eceaeb3399ef0be19
javoor1 <javoor1@hotmail.com>
2018-09-25 19:25:06 -0500

    audit log automatic weekly creation

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'admin dashboard' do
-  xit 'does not allow users to access without being signed in' do
+  it 'does not allow users to access without being signed in' do
     visit admin_root_path
     expect(current_path).to eq(new_user_session_path)
   end
